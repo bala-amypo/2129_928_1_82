25d47cb65732b7f1ea8169e3a737c0424951905b
bala-amypo <bala@amypo.in>
2025-12-18 10:06:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controller/ProductivityMetricRecordController.java
@@ -0,0 +1,42 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.Entity.ProductivityMetricRecord;
+import com.example.demo.Service.ProductivityMetricService;
+
+@RestController
+@RequestMapping("/api/metrics")
+public class ProductivityMetricController {
+
+    private final ProductivityMetricService service;
+
+    public ProductivityMetricController(ProductivityMetricService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ProductivityMetricRecord record(@RequestBody ProductivityMetricRecord m) {
+        return service.recordMetric(m);
+    }
+
+    @PutMapping("/{id}")
+    public ProductivityMetricRecord update(@PathVariable Long id,@RequestBody ProductivityMetricRecord m) {
+        return service.updateMetric(id, m);
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public List<ProductivityMetricRecord> byEmployee(@PathVariable Long employeeId) {
+        return service.getMetricsByEmployee(employeeId);
+    }
+
+    @GetMapping("/{id}")
+    public ProductivityMetricRecord get(@PathVariable Long id) {
+        return service.getMetricById(id);
+    }
+
+    @GetMapping
+    public List<ProductivityMetricRecord> all() {
+        return service.getAllMetrics();
+    }
+}
