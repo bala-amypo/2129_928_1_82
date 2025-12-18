e1a31fa718ef9619d400bc9707ffcd3322b37062
bala-amypo <bala@amypo.in>
2025-12-18 09:58:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/Respository/AnomalyFlagRecordRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.AnomalyFlagRecord;
+
+public interface AnomalyFlagRecordRepository extends JpaRepository<AnomalyFlagRecord, Long> {
+
+    List<AnomalyFlagRecord> findByMetricId(Long metricId);
+}
