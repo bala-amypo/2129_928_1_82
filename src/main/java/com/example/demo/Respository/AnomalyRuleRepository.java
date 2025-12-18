74d8c55d79ead60702baf88619962083aea34471
bala-amypo <bala@amypo.in>
2025-12-18 09:58:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/Respository/AnomalyRuleRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.AnomalyRule;
+
+public interface AnomalyRuleRepository extends JpaRepository<AnomalyRule, Long> {
+
+    List<AnomalyRule> findByActiveTrue();
+}
