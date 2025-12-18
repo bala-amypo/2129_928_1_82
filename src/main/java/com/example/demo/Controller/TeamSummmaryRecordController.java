42979853bcbab59f2da71eb2cdec51508f553b6d
bala-amypo <bala@amypo.in>
2025-12-18 10:14:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controller/TeamSummmaryRecordController.java
@@ -0,0 +1,32 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.Entity.TeamSummaryRecord;
+import com.example.demo.Service.TeamSummaryService;
+
+@RestController
+@RequestMapping("/api/team-summaries")
+public class TeamSummaryController {
+
+    private final TeamSummaryService service;
+
+    public TeamSummaryController(TeamSummaryService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/generate")
+    public TeamSummaryRecord generate(@RequestParam String teamName) {
+        return service.generateSummary(teamName);
+    }
+
+    @GetMapping("/team/{teamName}")
+    public List<TeamSummaryRecord> byTeam(@PathVariable String teamName) {
+        return service.getSummariesByTeam(teamName);
+    }
+
+    @GetMapping
+    public List<TeamSummaryRecord> all() {
+        return service.getAllSummaries();
+    }
+}
