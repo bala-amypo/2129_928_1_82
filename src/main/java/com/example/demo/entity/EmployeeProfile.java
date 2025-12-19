84a58b9fafcbd6c7a52f0486f2d7c49b72421856
bala-amypo <bala@amypo.in>
2025-12-19 05:14:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/EmployeeProfile.java
+++ src/main/java/com/example/demo/entity/EmployeeProfile.java
@@ -35,17 +35,43 @@ public class EmployeeProfile {
 
     public EmployeeProfile() {}
 
-    public Long getId() { return id; }
-    public String getEmployeeId() { return employeeId; }
-    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getTeamName() { return teamName; }
-    public void setTeamName(String teamName) { this.teamName = teamName; }
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    public Long getId() { 
+        return id; 
+    }
+    public String getEmployeeId() { 
+     return employeeId; 
+    }
+    public void setEmployeeId(String employeeId) {
+         this.employeeId = employeeId; 
+    }
+    public String getFullName() {
+         return fullName; 
+    }
+    public void setFullName(String fullName) {
+         this.fullName = fullName; 
+    }
+    public String getEmail() {
+         return email; 
+    }
+    public void setEmail(String email) {
+         this.email = email; 
+    }
+    public String getTeamName() {
+         return teamName; 
+    }
+    public void setTeamName(String teamName) {
+         this.teamName = teamName; 
+    }
+    public String getRole() {
+         return role;
+    }
+    public void setRole(String role) {
+         this.role = role;
+    }
+    public Boolean getActive() {
+         return active; 
+    }
+    public void setActive(Boolean active) {
+         this.active = active; 
+    }
 }
