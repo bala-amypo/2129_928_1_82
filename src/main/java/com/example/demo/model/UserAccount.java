dbb364507c159c0080af804c1dee76b1370cf545
bala-amypo <bala@amypo.in>
2025-12-29 06:12:32 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -30,14 +30,12 @@ public class UserAccount {
         createdAt = LocalDateTime.now();
     }
 
-    // getters & setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    // ✅ renamed methods
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
